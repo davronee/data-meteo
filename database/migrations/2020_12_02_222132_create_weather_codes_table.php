23673db9462b6cdbf23be7a234a40dcc67756c2d
davronee@gmail.com <cda7q14r>
2020-12-02 22:53:40 +0500

    loaded from meteo.uz

---
+++ database/migrations/2020_12_02_222132_create_weather_codes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeatherCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weather_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique()->nullable();
+            $table->string('name_ru')->nullable();
+            $table->timestamps();
+        });
+
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weather_codes');
+    }
+}
