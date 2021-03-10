b07de09f77e02c48b8d4383111e24f78dff9aa3e
davronee@gmail.com <cda7q14r>
2021-03-10 10:33:26 +0500

    `widthet changed`

--- app/Http/Controllers/ArmController.php
+++ app/Http/Controllers/ArmController.php
@@ -22,18 +22,18 @@ class ArmController extends Controller
            $exp = explode(';',$raw->data);
 
            $object = [
-               'date'=>$exp[0],
-               'time'=>$exp[1],
-               'type'=>$exp[2],
-               'id'=>$exp[3],
-               'voltage'=>$exp[4],
-               'fraction_concentration_pm2,5'=>$exp[5],
-               'fraction_concentration_pm10'=>$exp[6],
-               'temp'=>$exp[9],
-               'atmospheric_pressure'=>$exp[10],
-               'outdoor_temp'=>$exp[11],
-               'air_humidity'=>$exp[12],
-               'comment'=>$exp[13],
+               'date'=>$exp[0] , // дата
+               'time'=>$exp[1], // время
+               'type'=>$exp[2], // тир
+               'id'=>$exp[3], // ID
+               'voltage'=>$exp[4], // naprejeniya  na vihode fotometra v rejime produva (produv mB)
+               'fraction_concentration_pm2,5'=>$exp[5], // konsentratsiya fraksii rm2,5 v mkg/m 3
+               'fraction_concentration_pm10'=>$exp[6], // konsentratsiya fraksii rm10 v mkg/m 3
+               'temp'=>$exp[9], //temprature v bloke fotometra
+               'atmospheric_pressure'=>$exp[10], // temprature atmasfernogo davleniya
+               'outdoor_temp'=>$exp[11],// tekushaya znacheniya narujnogo vozduha
+               'air_humidity'=>$exp[12], // tekushaya znaheniya otnositelna  lajnosti vozduxa
+               'comment'=>$exp[13], // primicheniya
            ];
 
            array_push($array,$object);
