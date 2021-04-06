ba9eb6861c4d7ec97c806b7d53773c622a0d8752
davronee@gmail.com <cda7q14r>
2021-04-06 19:08:51 +0500

    widthet changed

--- resources/views/pages/calcite_maps/index.blade.php
+++ resources/views/pages/calcite_maps/index.blade.php
@@ -8,24 +8,24 @@
     <meta name="description" content="Метеорологическая карта Узбекистана">
     <meta name="author" content="">
     <!--favicon-->
-{{--    <link rel="apple-touch-icon" sizes="57x57" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-57x57.png">--}}
-{{--    <link rel="apple-touch-icon" sizes="60x60" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-60x60.png">--}}
-{{--    <link rel="apple-touch-icon" sizes="72x72" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-72x72.png">--}}
-{{--    <link rel="apple-touch-icon" sizes="76x76" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-76x76.png">--}}
-{{--    <link rel="apple-touch-icon" sizes="114x114" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-114x114.png">--}}
-{{--    <link rel="apple-touch-icon" sizes="120x120" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-120x120.png">--}}
-{{--    <link rel="apple-touch-icon" sizes="144x144" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-144x144.png">--}}
-{{--    <link rel="apple-touch-icon" sizes="152x152" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-152x152.png">--}}
-{{--    <link rel="apple-touch-icon" sizes="180x180" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-180x180.png">--}}
-{{--    <link rel="icon" type="image/png" sizes="192x192" href="https://hydromet.uz/templates/meteouz/images/favicon/android-icon-192x192.png">--}}
-{{--    <link rel="icon" type="image/png" sizes="32x32" href="https://hydromet.uz/templates/meteouz/images/favicon/favicon-32x32.png">--}}
-{{--    <link rel="icon" type="image/png" sizes="96x96" href="https://hydromet.uz/templates/meteouz/images/favicon/favicon-96x96.png">--}}
-{{--    <link rel="icon" type="image/png" sizes="16x16" href="https://hydromet.uz/templates/meteouz/images/favicon/favicon-16x16.png">--}}
-{{--    <link rel="shortcut icon" href="https://hydromet.uz/templates/meteouz/images/favicon/favicon.ico">--}}
-{{--    <link rel="manifest" href="https://hydromet.uz/templates/meteouz/images/favicon/manifest.json">--}}
+    <link rel="apple-touch-icon" sizes="57x57" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="https://hydromet.uz/templates/meteouz/images/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="https://hydromet.uz/templates/meteouz/images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="https://hydromet.uz/templates/meteouz/images/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="https://hydromet.uz/templates/meteouz/images/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="https://hydromet.uz/templates/meteouz/images/favicon/favicon-16x16.png">
+    <link rel="shortcut icon" href="https://hydromet.uz/templates/meteouz/images/favicon/favicon.ico">
+    <link rel="manifest" href="https://hydromet.uz/templates/meteouz/images/favicon/manifest.json">
     <meta name="yandex-verification" content="c194b7ef7003b9b1" />
     <meta name="msapplication-TileColor" content="#ffffff">
-{{--    <meta name="msapplication-TileImage" content="https://hydromet.uz/templates/meteouz/images/favicon/ms-icon-144x144.png">--}}
+    <meta name="msapplication-TileImage" content="https://hydromet.uz/templates/meteouz/images/favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
     <!--favicon-->
     <title>METEO MONITORING MAP - UZHYDROMET</title>
