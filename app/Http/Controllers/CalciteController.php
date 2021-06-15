863482136f051c46aa6df2b4ab82372712300307
davronee <davronee@gmail.com>
2021-06-15 23:51:09 +0500

    Karakalpak stations added

--- app/Http/Controllers/CalciteController.php
+++ app/Http/Controllers/CalciteController.php
@@ -41,7 +41,6 @@ class CalciteController extends Controller
     {
         $hydrometStations = HydrometStation::where('is_active', true)->with('hydromet_sensor_data')->get();
 
-        return $hydrometStations;
 
         return view('pages.hydromet_map')->with([
             'hydrometstation' => $hydrometStations
