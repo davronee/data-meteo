7e1baf553da119b0352b96dd3e7024294871b7c2
root <root@localhost.localdomain>
2021-08-24 16:06:17 +0500

    commited

--- resources/views/pages/calcite_maps/index.blade.php
+++ resources/views/pages/calcite_maps/index.blade.php
@@ -3114,7 +3114,21 @@
 </script>
 {{--<script src="{{asset('calcite/js/jquery/calcitemaps-v0.10.js')}}"/>--}}
 
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
+   ym(83964367, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/83964367" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
