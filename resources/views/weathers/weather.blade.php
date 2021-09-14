7d504ec2f7a1d1f0ea7bb5d32c98accdf27afdc9
Davron Ergashev <davronee@gmail.com>
2021-09-14 10:59:28 +0500

    Meteoinfocom Television  stations added

--- resources/views/weathers/weather.blade.php
+++ resources/views/weathers/weather.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport"
             content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
+        <title>ForeCast</title>
         <!-- icons css -->
         <link href="{{asset('template/lib/fontawesome5/css/all.min.css')}}" rel="stylesheet">
         <link rel="stylesheet" href="{{asset('template/assets/css/weather-icons.min.css')}}">
