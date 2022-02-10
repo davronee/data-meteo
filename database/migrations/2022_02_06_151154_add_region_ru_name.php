899bd3b88022d73e48ad5bf84ca1fecceed1ff2a
Davron Ergashev <davronee@gmail.com>
2022-02-11 01:01:46 +0500

    commited

---
+++ database/migrations/2022_02_06_151154_add_region_ru_name.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRegionRuName extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('weather_regions', function (Blueprint $table) {
+            $table->string('name_ru')->nullable();
+            $table->string('name_uz')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('weather_regions', function (Blueprint $table) {
+            $table->dropColumn('name_ru');
+            $table->dropColumn('name_uz');
+        });
+    }
+}
