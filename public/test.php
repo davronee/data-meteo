82070f697dd90ddd1e5f97d7d3ab079176070b2b
davronee <davronee@gmail.com>
2022-02-11 16:24:11 +0500

    commited

---
+++ public/test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<iframe src="https://www.meteoblue.com/en/weather/maps/tashkent_uzbekistan_1512569?cloudsAndPrecipitation=1&geoloc=fixed&tempunit=C&windunit=km%252Fh&lengthunit=metric&zoom=5&autowidth=auto" style="width: 100%; height: 720px"></iframe>
+</body>
+</html>
