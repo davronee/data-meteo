ef0188c917af0fe8ee62e5e0f8329fdf5ae9e747
Davron Ergashev <davronee@gmail.com>
2022-06-12 19:32:24 +0500

    added OpenservicesReestr

--- resources/views/pages/service/service_register.blade.php
+++ resources/views/pages/service/service_register.blade.php
@@ -17,34 +17,36 @@
                     <div class="tab-content">
                         <h1> Мурожаатлар очиқ реестри</h1>
                         <div class="row">
-                            <table class="table table-bordered">
-                                <thead>
-                                <tr>
-                                    <th scope="col">т/р</th>
-                                    <th scope="col">Ариза рақами</th>
-                                    <th scope="col">Давлат хизмати номи</th>
-                                    <th scope="col">Келиб тушган сана</th>
-                                    <th scope="col">Мурожаатчи ФИО си</th>
-                                    <th scope="col">Ижрочи</th>
-                                    <th scope="col">Ижрочи телефон рақами</th>
-                                    <th scope="col">Ҳолати</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($offers as $key=>$offer)
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <thead>
                                     <tr>
-                                        <th scope="row">  {{ $offers->firstItem() + $key }}</th>
-                                        <td>{!! $offer->id !!}</td>
-                                        <td>{!! $offer->service->name !!}</td>
-                                        <td>{!! $offer->created_at->format('d.m.Y') !!}</td>
-                                        <td>{!! $offer->fio !!}</td>
-                                        <td>Д. Қориев</td>
-                                        <td>Д. Қориев</td>
-                                        <td>Кўриб чиқилмоқда</td>
+                                        <th scope="col">т/р</th>
+                                        <th scope="col">Ариза рақами</th>
+                                        <th scope="col">Давлат хизмати номи</th>
+                                        <th scope="col">Келиб тушган сана</th>
+                                        <th scope="col">Мурожаатчи ФИО си</th>
+                                        <th scope="col">Ижрочи</th>
+                                        <th scope="col">Ижрочи телефон рақами</th>
+                                        <th scope="col">Ҳолати</th>
                                     </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                                    </thead>
+                                    <tbody>
+                                    @foreach($offers as $key=>$offer)
+                                        <tr>
+                                            <th scope="row">  {{ $offers->firstItem() + $key }}</th>
+                                            <td>{!! $offer->id !!}</td>
+                                            <td>{!! $offer->service->name !!}</td>
+                                            <td>{!! $offer->created_at->format('d.m.Y') !!}</td>
+                                            <td>{!! $offer->fio !!}</td>
+                                            <td>Д. Қориев</td>
+                                            <td>Д. Қориев</td>
+                                            <td>Кўриб чиқилмоқда</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                             {!! $offers->render() !!}
                         </div>
                     </div>
