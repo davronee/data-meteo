b45e433044deffebbf5cacd655a0a1fb5486d0a1
Davron Ergashev <davronee@gmail.com>
2022-06-13 17:42:32 +0500

    added OpenservicesReestr

--- resources/views/pages/calcite_maps/index.blade.php
+++ resources/views/pages/calcite_maps/index.blade.php
@@ -2321,16 +2321,8 @@
                                     "<td>" + response.data.data[0].dataItem[5].registerItem[1].data + ' ' + response.data.data[0].dataItem[5].registerItem[1].unit + "</td>" +
                                     "</tr>" +
                                     "<tr>" +
-                                    "<td><b>EC</b></td>" +
-                                    "<td>" + response.data.data[0].dataItem[6].registerItem[0].data + ' ' + response.data.data[0].dataItem[6].registerItem[0].unit + "</td>" +
-                                    "</tr>" +
-                                    "<tr>" +
                                     "<td><b>совокупное количество осадков</b></td>" +
                                     "<td>" + response.data.data[0].dataItem[7].registerItem[0].data + ' ' + response.data.data[0].dataItem[7].registerItem[0].unit + "</td>" +
-                                    "</tr>" +
-                                    "<tr>" +
-                                    "<td><b>RA</b></td>" +
-                                    "<td>" + response.data.data[0].dataItem[8].registerItem[0].data + ' ' + response.data.data[0].dataItem[8].registerItem[0].unit + "</td>" +
                                     "</tr>" +
                                     "<tr>" +
                                     "<td><b>температура листа</b></td>" +
