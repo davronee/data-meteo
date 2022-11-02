f2ddcb956d7092ac94f59f1f9e23d6f85029ed5a
Davron Ergashev <davronee@gmail.com>
2022-11-02 15:00:34 +0500

    added meteobot new

--- resources/views/pages/calcite_maps/index.blade.php
+++ resources/views/pages/calcite_maps/index.blade.php
@@ -3649,7 +3649,7 @@
                                     .on('click', function () {
                                         marker2.bindPopup("" +
                                             "<table class='table table-bordered'>" +
-                                            "<tr ><td colspan='2' class='text-center'><b>Ходжейли</b></td></tr>" +
+                                            "<tr ><td colspan='2' class='text-center'><b>Қўнғирот</b></td></tr>" +
                                             "<tr>" +
                                             "<td><b>@lang('map.date')</b></td>" +
                                             "<td>" + response.data[1] + " " + response.data[2] + "</td>" +
