41c6a21c0e0445869eb415836309b38f5d9b246a
Davron Ergashev <davronee@gmail.com>
2022-12-06 16:29:48 +0500

    added meteobot new

---
+++ app/Console/Commands/ImsGetFacticConsole.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Classes\Services;
+use Illuminate\Console\Command;
+
+class ImsGetFacticConsole extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ims:faktik';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $service = new Services();
+        $service->GetFaktikIms();
+    }
+}
